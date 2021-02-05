1551b66bec78b706fb496a1c6b0c079897477a05
damiantomczyszyn <65863645+damiantomczyszyn@users.noreply.github.com>
2021-02-05 18:58:24 +0100

    UPDATE 4
    świeci spoko
    gdy sekundy przeskakuja z 9 na 10 to wywala pierwszy wyświetlacz wartość jest =10 nie jest zerowana nwm czemu ;/
    sprawdzić czy z minutami i 2 cyfrą sekund jest tak samo

--- proj.c
+++ proj.c
@@ -3,7 +3,7 @@
 
 #include <8051.h>
 __bit __at(0x97) LED;
-
+__bit __at (0x95) BUZZER;
 __bit __at(0xB5) kbt1;//p3.5
 __sbit __at (0x96) seg;
 __sbit __at (0XB5) T1;
@@ -14,8 +14,10 @@ __bit sendflag=0; // dane gotowe do transmisji
 __bit migflag=0;
 __bit edycja=0;
 __bit nieodsw=1;
+__bit niezmienia=0;
 unsigned char indeks = 0,ktoryedytowany=0;
-unsigned int licznik=0,wyswietlana =0;
+unsigned int licznik=0; 
+unsigned char wyswietlana =0;
 unsigned int indeks1=0;
 
 
@@ -44,7 +46,7 @@ indeks=0b00000001;
 wyswietlana = 0;
 ;
 
-
+	niezmienia=0;
 	while(indeks!=0b01000000)
 	{
 
@@ -66,10 +68,12 @@ wyswietlana=4;   }
                  seg = 1;  //wy³¹cz
 		*buf_CSDS = indeks;
 		*buf_CSDB = Cyfry[liczbystartowe[wyswietlana]];
+	//	if(liczbystartowe[wyswietlana]==10)
+	//BUZZER=0;
                 seg = 0; //w³¹cz
-	      y=0;
-	 while(y<5)
-	  y++;
+	     // y=0;
+	// while(y<5)
+	 // y++;
 
             	wyswietlana++;
 
@@ -78,6 +82,7 @@ wyswietlana=4;   }
             	//seg = 0;  //w³¹cz
         }
             seg = 1;  //wy³¹cz
+            niezmienia=1;
 	 }
 
 
@@ -132,7 +137,7 @@ EA=1;
 void TIME()
 {
 
-	if (liczbystartowe[0]+1==10)          // jeœli 1 zanak sek przeskakuje na 10 to
+	if (liczbystartowe[0]==9)          // jeœli 1 zanak sek przeskakuje na 10 to
 	{
 	liczbystartowe[0]=0;          //zmieñ go na zero      kk
 
@@ -202,17 +207,15 @@ trybedycji[5]=liczbystartowe[5];
 ktoryedytowany=0;
 TH0 = 253;
 licznik=0;
+
+
 while(1)  //trybedycjiu
 {
-
-nieodsw=1;
-    y=0;
- while(y<5)
-  y++;
+	nieodsw=1;
+y=0;
+while(y<30)
+y++;
 nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
 
 
 indeks1=0b00000001;
@@ -223,89 +226,42 @@ while(i!=6)
 	{
 
 
- nieodsw=0;
+if(niezmienia){
 *buf_CSDS = indeks1;
 
-if(klawmultiplekss!=0) //jeœli cos jest klikniête sprawdzamy czy zosta³o odklikniête
+if(klawmultiplekss!=0b00000000) //jeœli cos jest klikniête sprawdzamy czy zosta³o odklikniête
 {
-if((klawmultiplekss&0b00000001)==(indeks1)&&kbt1==0)//odklikniêty
-klawmultiplekss&=0b11111110;
+if((klawmultiplekss&0b00000001)==(indeks1)&&kbt1==0)//odklikniêty enter
+klawmultiplekss&=   0b11111110;
 
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
-
-if((klawmultiplekss&0b00000010)==(indeks1)&&kbt1==0)//odklikniêty
-klawmultiplekss&=0b11111101;
-
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
-
-if((klawmultiplekss&0b00000100)==(indeks1)&&kbt1==0)//odklikniêty
-klawmultiplekss&=0b11111011;
 
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
+if((klawmultiplekss&0b00000010)==(indeks1)&&kbt1==0)//odklikniêty  esc
+klawmultiplekss&=   0b11111101;
 
 
-if((klawmultiplekss&0b00001000)==(indeks1)&&kbt1==0)//odklikniêty
-klawmultiplekss&=0b11110111;
+if((klawmultiplekss&0b00000100)==(indeks1)&&kbt1==0)//odklikniêty   prawo
+klawmultiplekss&=   0b11111011;
 
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<5)
-  y++;
 
-if((klawmultiplekss&0b00010000)==(indeks1)&&kbt1==0)//odklikniêty
-klawmultiplekss&=0b11101111;
+if((klawmultiplekss&0b00001000)==(indeks1)&&kbt1==0)//odklikniêty   góra
+klawmultiplekss&=   0b11110111;
 
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
+
+if((klawmultiplekss&0b00010000)==(indeks1)&&kbt1==0)//odklikniêty  dp;
+klawmultiplekss&=   0b11101111;
 
 
-if((klawmultiplekss&0b00100000)==(indeks1)&&kbt1==0)//odklikniêty
-klawmultiplekss&=0b11011111;
-nieodsw=1;
+
+if((klawmultiplekss&0b00100000)==(indeks1)&&kbt1==0)//odklikniêty   lewo
+klawmultiplekss&=   0b11011111;
+//nieodsw=1;
 } else//jeœli ==0 to nic nie jest wciœniête i mo¿emy coœ przycisn¹æ
-{   nieodsw=1;
-	 y=0;
- while(y<5)
-  y++;
- nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
+{   //nieodsw=1;
+
 if(indeks1==    0b00000001&&kbt1==1){  //wciœniêty  enter
 klawmultiplekss=0b00000001;
 LED^=1;          
@@ -339,59 +295,163 @@ break;
 }
 
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
+
 
 if(indeks1==    0b00000100&&kbt1==1){  //wciœniêty    PRAWO
 klawmultiplekss=0b00000100;
 
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
 
 if(ktoryedytowany!=0)
 ktoryedytowany--;
 LED^=1;           }
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
 
 
 if(indeks1==    0b00001000&&kbt1==1){  //wciœniêty     GÓRA
 klawmultiplekss=0b00001000;
+ //nieodsw=0;
+if (ktoryedytowany == 1)
+goto minuty;
+if (ktoryedytowany == 2)
+goto godziny;
+if (ktoryedytowany == 0)//to sekundy
+{
+
+
+	if (liczbystartowe[0] + 1 == 10)          // jeœli 1 zanak sek przeskakuje na 10 to
+	{     //BUZZER=0;
+
+		liczbystartowe[0] = 0;          //zmieñ go na zero      kk
+
+		if (liczbystartowe[1] + 1 == 6)            //i jeœli w tym czasie przeskakuje 2gi znak sek to zmieñ go na zero kk
+		{
+			liczbystartowe[1] = 0;
+		minuty:
+			if (liczbystartowe[2] + 1 == 10)          // jeœli 1 zanak min przeskakuje na 10 to
+			{
+				liczbystartowe[2] = 0;          //zmieñ go na zero
+
+				if (liczbystartowe[3] + 1 == 6)            //i jeœli w tym czasie przeskakuje 2gi znak min to zmieñ go na zero
+				{
+					liczbystartowe[3] = 0;
+
+				godziny:
+					if ((liczbystartowe[4] + 1 == 4) && (liczbystartowe[5] == 2))//jeœli przeskakuje na 24 to godzina = 00
+					{
+						liczbystartowe[4] = 0;
+						liczbystartowe[5] = 0;
+					}
+					else                  //jeœli nie to
+
+
+						if (liczbystartowe[4] + 1 == 10)          // jeœli 1 zanak godz przeskakuje na 10 to jeœli przeskakuje na 4 to sprawdz czy nie ma 23
+						{
+							liczbystartowe[4] = 0;          //zmieñ go na zero
+							liczbystartowe[5]++;           //oraz zwiêksz 2gi znak godziny
+						}
+
+						else
+							liczbystartowe[4]++;         //gdy przeskakuje pierwszy znak godz bez przepe³nienia
+
+
+
+				}
+				else     // gdy przeskakuje 2gi znak sek   bez przepe³nienia
+					liczbystartowe[3]++;
+
+
+			}
+			else
+				liczbystartowe[2]++;         //gdy przeskakuje pierwszy znak sek bez przepe³nienia
+
+
+		}
+		else     // gdy przeskakuje 2gi znak sek   bez przepe³nienia
+			liczbystartowe[1]++;
+
+	}
+	else                                                                     // kk
+		liczbystartowe[0]++;         //gdy przeskakuje pierwszy znak sek bez przepe³nienia
+}
+
+
+
+    //nieodsw=1;
+
+
 LED^=1;           }
 
 if(indeks1==    0b00010000&&kbt1==1){  //wciœniêty     DÓ£
 klawmultiplekss=0b00010000;
+ //nieodsw=0;
+if (ktoryedytowany == 1)
+goto minutydol;
+if (ktoryedytowany == 2)
+goto godzinydol;
+if (ktoryedytowany == 0)//to sekundy
+{
+
+
+	if (liczbystartowe[0] == 0)          // jeœli 1 zanak sek przeskakuje na 10 to
+	{
+		liczbystartowe[0] = 9;          //zmieñ go na zero      kk
+
+		if (liczbystartowe[1] == 0)            //i jeœli w tym czasie przeskakuje 2gi znak sek to zmieñ go na zero kk
+		{
+			liczbystartowe[1] = 5;
+		minutydol:
+			if (liczbystartowe[2] == 0)          // jeœli 1 zanak min przeskakuje na 10 to
+			{
+				liczbystartowe[2] = 9;          //zmieñ go na zero
+
+				if (liczbystartowe[3] == 0)            //i jeœli w tym czasie przeskakuje 2gi znak min to zmieñ go na zero
+				{
+					liczbystartowe[3] = 5;
+
+				godzinydol:
+					if ((liczbystartowe[4] == 0) && (liczbystartowe[5] == 0))//jeœli przeskakuje na 24 to godzina = 00
+					{
+						liczbystartowe[4] = 3;
+						liczbystartowe[5] = 2;
+					}
+					else                  //jeœli nie to
+
+
+						if (liczbystartowe[4] == 0)          // jeœli 1 zanak godz przeskakuje na 10 to jeœli przeskakuje na 4 to sprawdz czy nie ma 23
+						{
+							liczbystartowe[4] = 9;          //zmieñ go na zero
+							liczbystartowe[5]--;           //oraz zwiêksz 2gi znak godziny
+						}
+
+						else
+							liczbystartowe[4]--;         //gdy przeskakuje pierwszy znak godz bez przepe³nienia
+
+
+
+				}
+				else     // gdy przeskakuje 2gi znak sek   bez przepe³nienia
+					liczbystartowe[3]--;
+
+
+			}
+			else
+				liczbystartowe[2]--;         //gdy przeskakuje pierwszy znak sek bez przepe³nienia
+
+
+		}
+		else     // gdy przeskakuje 2gi znak sek   bez przepe³nienia
+			liczbystartowe[1]--;
+
+	}
+	else                                                                     // kk
+		liczbystartowe[0]--;         //gdy przeskakuje pierwszy znak sek bez przepe³nienia
+}
+
+   //nieodsw=1;
+
 LED^=1;           }
 
-
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<2)
-  y++;
 
 
 
@@ -404,16 +464,16 @@ ktoryedytowany++;
 
 
 
-nieodsw=1;
+//nieodsw=1;
 }
 
 
 indeks1 = indeks1 << 1;
 i++;
  }
-nieodsw=1;
+//nieodsw=1;
 
-
+}
 }
 wyjdz:
 }  //koniec tr edycji-obsluga
@@ -430,14 +490,6 @@ indeks1=0b00000001;
 while(i!=6)
 	{
 
-nieodsw=1;
- y=0;
- while(y<5)
-  y++;
-nieodsw=0;
-  y=0;
- while(y<5)
-  y++;
 *buf_CSDS = indeks1;
 
 //EA=0;
@@ -446,16 +498,16 @@ if((indeks1==0b00000100||indeks1==0b00100000)&&kbt1==1)//klikniêty   LEWO  LUB P
 {
 klawmultiplekss=indeks1;
 
-nieodsw=1;
+//nieodsw=1;
 OBSLUGA();//trybedycji
  }
  wyjdz:
- nieodsw=1;
+ //nieodsw=1;
 indeks1 = indeks1 << 1;
 i++;
 
 }
-nieodsw=1;
+//nieodsw=1;
 }
 
 
